c40db5e051f6e6bfb0c4d4e4e4ab2d9b6508350e
Toderita Mihai <mihai.toderita@stud.acs.upb.ro>
2025-12-03 16:43:08 +0200

    Added new world
    World name: 'world10.txt'

--- src/states.py
+++ src/states.py
@@ -326,11 +326,12 @@ class Gameplay(State):
     def __init__(self, game):
         super().__init__(game)
         # Create worlds once
-        world_level_01 = create_world("../assets/worlds/world9.txt")
+        world_level_01 = create_world("../assets/worlds/world10.txt")
         world_level_02 = create_world("../assets/worlds/world2.txt")
         world_level_03 = create_world("../assets/worlds/world3.txt")
         world_level_04 = create_world("../assets/worlds/world4.txt")
-        world_level_05 = create_world("../assets/worlds/world5.txt")
+        world_level_05 = create_world("../assets/worlds/world9.txt")
+        world_level_06 = create_world("../assets/worlds/world5.txt")
         secret_world = create_world("../assets/worlds/secret.txt")
         secret_world1 = create_world("../assets/worlds/secret1.txt")
 
@@ -340,7 +341,8 @@ class Gameplay(State):
             ("../assets/backgrounds/sky.jpg", world_level_02, 2, 7 * BLOCK_SIZE, SCREEN_HEIGHT - 7 * BLOCK_SIZE),
             ("../assets/backgrounds/sky.jpg", world_level_03, 3, 5 * BLOCK_SIZE, SCREEN_HEIGHT / 4),
             ("../assets/backgrounds/sky.jpg", world_level_04, 4, 5 * BLOCK_SIZE, SCREEN_HEIGHT - 2 * BLOCK_SIZE),
-            ("../assets/backgrounds/sky.jpg", world_level_05, 5, 6 * BLOCK_SIZE, SCREEN_HEIGHT - 13 * BLOCK_SIZE)
+            ("../assets/backgrounds/sky.jpg", world_level_05, 5, 6 * BLOCK_SIZE, SCREEN_HEIGHT - 13 * BLOCK_SIZE),
+            ("../assets/backgrounds/sky.jpg", world_level_06, 4, 5 * BLOCK_SIZE, SCREEN_HEIGHT - 2 * BLOCK_SIZE)
         ]
         self.secret_world_sequence = [
             # The last level is only accesible by interacting with the All Powerful Acorn
